6bbfa0187fa99002341f80aaf082c5c33164b3e4
Israel Cohen <josh50170@gmail.com>
2023-03-16 14:10:33 +0200

    add ducomentation

--- Ex1/5.1.that_is_the_way.py
+++ Ex1/5.1.that_is_the_way.py
@@ -3,5 +3,10 @@ import os
 
 
 def get_files(path):
+    """
+    Find all files in a specific directory which are start in 'deep'
+    :param path: Path to directory
+    :return: List of All files in the directory which are start in 'deep'
+    """
     files = os.listdir(path)
     return [name for name in files if re.match('^deep', name)]
