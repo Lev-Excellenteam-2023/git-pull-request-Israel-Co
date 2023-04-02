3b9ca52cc84ebec98c62dbc8c1287afd670a414f
Israel Cohen <josh50170@gmail.com>
2023-04-02 23:04:22 +0300

    add ducometation and finish Ex. 2k run

--- Ex2/6.2.2k_run.py
+++ Ex2/6.2.2k_run.py
@@ -3,9 +3,16 @@ from functools import reduce
 
 
 def timer(f, *args):
+    """
+    Measure how long is the running time of function f with parameters in which are in args
+    :param f: function
+    :param args: Input parameters for f 
+    :return: The running time of f in seconds
+    """
     start_time = time.time()
     print(f(*args))
     return time.time() - start_time
 
 
+# running example
 print('Run time:', timer(reduce, lambda x, y: x * y, [1, 2, 3, 4, 5]), 'Sec')
